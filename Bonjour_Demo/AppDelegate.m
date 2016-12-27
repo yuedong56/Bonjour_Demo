fb51b7d374a0a68675560152a8171b29a04d52fc
yuedongkui <865812052@qq.com>
2016-12-27 18:50:26 +0800

    增加消息发送逻辑

--- Bonjour_Demo/AppDelegate.m
+++ Bonjour_Demo/AppDelegate.m
@@ -7,14 +7,20 @@
 //
 
 #import "AppDelegate.h"
+#import "GCDAsyncSocket.h"
 
 #define kDomain @"local."
 #define kServiceType @"_ProbeHttpService._tcp."
 
 
 @interface AppDelegate ()<NSNetServiceDelegate>
+{
+    NSNetService *netService;
+    GCDAsyncSocket *asyncSocket;
+    GCDAsyncSocket *receiveSocket;
+    NSMutableArray *connectedSockets;
+}
 @property (weak) IBOutlet NSWindow *window;
-@property (strong) NSNetService *netService;
 @end
 
 
@@ -22,21 +28,63 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    //1.创建服务类
-    self.netService = [[NSNetService alloc] initWithDomain:kDomain type:kServiceType name:@"texeDemo" port:8888];
-    [self.netService scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
-    self.netService.delegate = self;
-    [self.netService publish];
-    NSLog(@"创建服务类");
+    
+    connectedSockets = [[NSMutableArray alloc] init];
+
+    //创建socket服务
+    asyncSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+    asyncSocket.delegate = self;
+    // Now we tell the socket to accept incoming connections.
+    // We don't care what port it listens on, so we pass zero for the port number.
+    // This allows the operating system to automatically assign us an available port.
+    // 如果端口为零，则自动监听可用的端口
+    NSError *err = nil;
+    if ([asyncSocket acceptOnPort:0 error:&err])
+    {
+        // So what port did the OS give us?
+        
+        UInt16 port = [asyncSocket localPort];
+        
+        // Create and publish the bonjour service.
+        // Obviously you will be using your own custom service type.
+        
+        //1.创建服务类
+        netService = [[NSNetService alloc] initWithDomain:kDomain
+                                                     type:kServiceType
+                                                     name:@""
+                                                     port:port];
+        [netService scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
+        [netService setDelegate:self];
+        [netService publish];
+        
+        NSLog(@"创建服务类");
+    }
+    else
+    {
+        NSLog(@"Error in acceptOnPort:error: -> %@", err);
+    }
 }
 
-#pragma mark -
-- (IBAction)sendTxt:(id)sender {
-    NSString *abc = @"1";
-    NSData *data = [abc dataUsingEncoding:NSUTF8StringEncoding];
-    [self.netService setTXTRecordData:data];
-    NSLog(@"data = %@", data);
-    _logLabel.stringValue = [NSString stringWithFormat:@"%@\n 发送消息 %@", _logLabel.stringValue, abc];
+#pragma mark - 发送消息
+//发送Txt消息
+- (IBAction)sendTxt:(id)sender
+{
+    // You can optionally add TXT record stuff
+    
+    NSMutableDictionary *txtDict = [NSMutableDictionary dictionaryWithCapacity:2];
+    NSString *num = [NSString stringWithFormat:@"%d", arc4random()%100];
+    [txtDict setObject:num forKey:@"num"];
+    [txtDict setObject:@"quack" forKey:@"duck"];
+    
+    NSData *txtData = [NSNetService dataFromTXTRecordDictionary:txtDict];
+    [netService setTXTRecordData:txtData];
+    
+    _logLabel.stringValue = [NSString stringWithFormat:@"%@\n 发送消息 %@", _logLabel.stringValue, txtDict];
+}
+
+//发送socket消息
+- (IBAction)sendSocketMsg:(id)sender {
+    
 }
 
 #pragma mark -
@@ -55,4 +103,37 @@
     _logLabel.stringValue = [NSString stringWithFormat:@"%@\nerror:发布失败, %@", _logLabel.stringValue, errorDict];
 }
 
+#pragma mark - GCDAsyncSocketDelegate
+- (void)socket:(GCDAsyncSocket *)sock didAcceptNewSocket:(GCDAsyncSocket *)newSocket
+{
+    NSLog(@"Accepted new socket from %@:%hu", [newSocket connectedHost], [newSocket connectedPort]);
+    
+    // The newSocket automatically inherits its delegate & delegateQueue from its parent.
+    
+    [connectedSockets addObject:newSocket];
+    
+    receiveSocket = newSocket;
+    [receiveSocket readDataWithTimeout:-1 tag:0];
+}
+
+- (void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(NSError *)err
+{
+    [connectedSockets removeObject:sock];
+}
+
+//收到消息
+- (void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag
+{
+    [receiveSocket readDataWithTimeout:-1 tag:0];
+
+    NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data
+                                                               options:NSJSONReadingMutableContainers
+                                                                 error:nil];
+    NSLog(@"dictionary ----- %@", dictionary);
+}
+
 @end
+
+
+
+
--- Bonjour_iOS/ViewController.m
+++ Bonjour_iOS/ViewController.m
@@ -7,45 +7,64 @@
 //
 
 #import "ViewController.h"
+#import "GCDAsyncSocket.h"
 
 #define kDomain @"local."
 #define kServiceType @"_ProbeHttpService._tcp."
 
-@interface ViewController ()<NSNetServiceBrowserDelegate, NSNetServiceDelegate>
-@property (nonatomic, strong) NSMutableArray *netServices;
+@interface ViewController ()<NSNetServiceBrowserDelegate, NSNetServiceDelegate, GCDAsyncSocketDelegate>
+{
+    NSNetServiceBrowser *netServiceBrowser;
+    NSNetService *serverService;
+    NSMutableArray *serverAddresses;
+
+    GCDAsyncSocket *asyncSocket;
+    BOOL connected;
+}
 @property (weak, nonatomic) IBOutlet UITextView *logLabel;
-@property (strong) NSNetServiceBrowser *serviceBrowser;
 @end
 
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     
-    self.netServices = [NSMutableArray array];
     //1.创建发现类
-    self.serviceBrowser = [[NSNetServiceBrowser alloc] init];
-    self.serviceBrowser.delegate = self;
+    netServiceBrowser = [[NSNetServiceBrowser alloc] init];
+    netServiceBrowser.delegate = self;
     //2.开始服务发现
-    [self.serviceBrowser searchForServicesOfType:kServiceType inDomain:kDomain];
-    //
+    [netServiceBrowser searchForServicesOfType:kServiceType inDomain:kDomain];
+    
+    //停止发现
+//    [netServiceBrowser stop];
+}
+
+#pragma mark - 发送socket消息
+- (IBAction)sendMessage:(id)sender {
+    
+    NSDictionary *dic = @{@"msg" : [NSString stringWithFormat:@"%d", arc4random()%100]};
+    NSData *data = [NSJSONSerialization dataWithJSONObject:dic options:NSJSONWritingPrettyPrinted error:nil];
+    [asyncSocket writeData:data withTimeout:1 tag:0];
 }
 
 #pragma mark -
 //发现服务
 - (void)netServiceBrowser:(NSNetServiceBrowser *)browser didFindService:(NSNetService *)service moreComing:(BOOL)moreComing
 {
-    //保存发现的服务
-    [self.netServices addObject:service];
-//    //开始监控是否有消息发过来（服务端是否执行setTXTRecordData:）
-//    [service startMonitoring];
-    service.delegate = self;
-    
-    //开始服务解析
-    [service resolveWithTimeout:5];
-    NSLog(@"===发现服务 %@", service);
-    _logLabel.text = [NSString stringWithFormat:@"%@\n===发现服务： %@", _logLabel.text, service];
+    if (serverService == nil) {
+        serverService = service;
+        service.delegate = self;
+        
+        //开始监控是否有消息发过来（服务端是否执行setTXTRecordData:）
+        [service startMonitoring];
+        
+        //开始服务解析
+        [service resolveWithTimeout:5];
+        NSLog(@"===发现服务 %@", service);
+        _logLabel.text = [NSString stringWithFormat:@"%@\n===发现服务： %@", _logLabel.text, service];
+    }
 }
 
 //未发现服务
@@ -55,13 +74,11 @@
     _logLabel.text = [NSString stringWithFormat:@"%@\n===未发现服务 error: %@", _logLabel.text, errorDict];
 }
 
-//收到消息
+//收到Txt消息
 - (void)netService:(NSNetService *)sender didUpdateTXTRecordData:(NSData *)data
 {
-    [sender startMonitoring];
-
-    NSString *abc = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    _logLabel.text = [NSString stringWithFormat:@"%@\n===收到新消息： %@", _logLabel.text, abc];
+    NSDictionary *infoDict = [NSNetService dictionaryFromTXTRecordData:data];
+    _logLabel.text = [NSString stringWithFormat:@"%@\n===收到新消息： %@", _logLabel.text, infoDict];
 }
 
 #pragma mark -
@@ -72,7 +89,79 @@
     NSLog(@"===服务获取到地址 hostName %@ prot %ld addressed %@", sender.hostName, sender.port, sender.addresses);
     
     _logLabel.text = [NSString stringWithFormat:@"%@\n===服务获取到地址 hostName %@ prot %ld addressed %@", _logLabel.text, sender.hostName, sender.port, sender.addresses];
+    
+    if (serverAddresses == nil)
+    {
+        serverAddresses = [[sender addresses] mutableCopy];
+    }
+
+    if (asyncSocket == nil)
+    {
+        asyncSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+        
+        [self connectToNextAddress];
+    }
 }
+
+#pragma mark - socket 部分
+- (void)connectToNextAddress
+{
+    BOOL done = NO;
+    
+    while (!done && ([serverAddresses count] > 0))
+    {
+        NSData *addr;
+        
+        // Note: The serverAddresses array probably contains both IPv4 and IPv6 addresses.
+        //
+        // If your server is also using GCDAsyncSocket then you don't have to worry about it,
+        // as the socket automatically handles both protocols for you transparently.
+        
+        if (YES) {// Iterate forwards
+            addr = [serverAddresses objectAtIndex:0];
+            [serverAddresses removeObjectAtIndex:0];
+        }
+//        else {// Iterate backwards
+//            addr = [serverAddresses lastObject];
+//            [serverAddresses removeLastObject];
+//        }
+        
+        NSLog(@"Attempting connection to %@", addr);
+        
+        NSError *err = nil;
+        if ([asyncSocket connectToAddress:addr error:&err]) {
+            done = YES;
+        }
+        else {
+            NSLog(@"Unable to connect: %@", err);
+        }
+    }
+    
+    if (!done) {
+        NSLog(@"Unable to connect to any resolved address");
+    }
+}
+
+#pragma mark -
+
+- (void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(UInt16)port
+{
+    NSLog(@"Socket:DidConnectToHost: %@ Port: %hu", host, port);
+    _logLabel.text = [NSString stringWithFormat:@"%@\n====Socket:DidConnectToHost: %@ Port: %hu", _logLabel.text, host, port];
+    connected = YES;
+}
+
+- (void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(NSError *)err
+{
+    NSLog(@"SocketDidDisconnect:WithError: %@", err);
+    _logLabel.text = [NSString stringWithFormat:@"%@\n====SocketDidDisconnect:WithError: %@", _logLabel.text, err];
+
+    if (!connected) {
+        [self connectToNextAddress];
+    }
+}
+
+
 
 @end
 
